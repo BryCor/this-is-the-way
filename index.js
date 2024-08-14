af3d7efdf82dc5c2dede0110594766fb73e2d253
Bryan Cortes <125423179+BryCor@users.noreply.github.com>
2024-08-14 21:53:17 +0000

    fix version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
-import * as core from '@actions/github';
+import * as github from '@actions/github';
 
 
 
@@ -8,7 +8,7 @@ try{
     const name = core.getInput('name');
     const output_value = (`This is the way ${name}! -the Mando and Grogu`);
     core.setOutput('greeting', 'output_value');
-    
+
 } catch (error) {
     core.setFailed(error.message);
   }
